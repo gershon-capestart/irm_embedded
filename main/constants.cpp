9222a7ee7a535004192f0cbde10db416410b30b2
Gershon <gershon.s@capestart.com>
2022-07-29 22:20:11 +0530

    init commit

--- main/constants.cpp
+++
@@ -1,6 +0,0 @@
-const char *mqtt_broker = "test.mosquitto.org";
-const char *topic = "9604123";
-const int mqtt_port = 1883;
-
-const char *ssid = "CapeStart"; 
-const char *password = "NunsP@EckyJ!ehWr";
